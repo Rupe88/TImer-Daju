731faae2c02a1a0446703c96ea6448c51ae9a373
Rupee88 <chyrupesh828@gmail.com>
2025-09-01 12:59:17 +0545

    Added changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    return this.authService.register(dto);
+  }
 }
