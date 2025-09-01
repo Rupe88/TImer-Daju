d0cfd70f38a0256c457be5421e8e3c422195eef6
Rupee88 <chyrupesh828@gmail.com>
2025-09-01 12:54:37 +0545

    Register Api Created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,36 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepo: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
+  //for register
+
+  async register(dto: CreateUserDto): Promise<User> {
+    const existing = await this.usersRepo.findOne({
+      where: { email: dto.email },
+    });
+    if (existing) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    const user = this.usersRepo.create({
+      ...dto,
+      password: hashedPassword,
+    });
+    return this.usersRepo.save(user);
+  }
   // ✅ This method is called by JwtStrategy
   async validateUser(payload: { id: number; email: string }): Promise<User> {
     const user = await this.usersRepo.findOne({ where: { id: payload.id } });
