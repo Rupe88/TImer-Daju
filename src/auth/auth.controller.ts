ff41295c403732945d1ab985b27955c39490aa41
Rupee88 <chyrupesh828@gmail.com>
2025-09-03 16:55:45 +0545

    feat: added profile and logout api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Res } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDTO } from './dto/login.dto';
 import type { Response } from 'express';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -28,5 +29,18 @@ export class AuthController {
     });
 
     return { user, token };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  profile(@Req() req: import('express').Request) {
+    return Promise.resolve(req?.user);
+  }
+
+  // ✅ Logout
+  @Post('logout')
+  logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('jwt');
+    return { message: 'Logged out successfully' };
   }
 }
